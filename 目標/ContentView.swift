c5c244af37e50e17e38a675116cbebb5715b8e09
st921 <syou.tana0921@icloud.com>
2026-03-06 17:35:36 +0900

    Initial Commit

---
+++ 目標/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  目標
+//
+//  Created by しょう on 2026/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 目標/__App.swift
@@ -0,0 +1,17 @@
+//
+//  __App.swift
+//  目標
+//
+//  Created by しょう on 2026/03/06.
+//
+
+import SwiftUI
+
+@main
+struct __App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
